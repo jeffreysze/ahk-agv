8fd3f19a56dd1d9b62fb72e0741606a9f615d0ec
MT Sze <szemt148@localhost.localdomain>
2024-06-04 02:22:40 +0800

    finish draft of demo 4june

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
 var createError = require('http-errors');
 var express = require('express');
+var hbs = require('hbs');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helpers = require('./components/hbsHelpers');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var mapRouter = require('./routes/map');
+var navigationRouter = require('./routes/navigation');
+var timetableRouter = require('./routes/timetable');
+var managementRouter = require('./routes/management');
+
+//const bootstrap = require('bootstrap')
 
 var app = express();
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'), (err) => {});
+for (let helper in helpers) {
+  hbs.registerHelper(helper, helpers[helper]);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/map', mapRouter);
+app.use('/navigation', navigationRouter);
+app.use('/timetable', timetableRouter);
+app.use('/management', managementRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', content: 'Test Site!' });
+  res.render('index', { title:'KYOEI Interface', email:'' });
 });
+
 
 module.exports = router;
